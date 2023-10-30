b3b46d010c05243fb300c35abdae4602d71ea040
IgorStrauss <engenheirostrauss2@gmail.com>
2023-10-30 14:35:34 -0300

    Formulario para cadastro de especialidades e medicos ok

--- control_clinic/views.py
+++ control_clinic/views.py
@@ -2,10 +2,18 @@ from flask import render_template
 
 
 def init_app(app):
-    @app.route("/")
+    @app.route("/", endpoint="index")
     def index():
         return render_template("base.html")
 
     @app.route("/cadastro/funcionario", endpoint="register_employee")
     def register_employee():
         return render_template("forms/register-employee.html")
+
+    @app.route("/cadastro/medico", endpoint="register_doctor")
+    def register_doctor():
+        return render_template("forms/register-doctor.html")
+
+    @app.route("/cadastro/especialidade", endpoint="register_specialty")
+    def register_specialty():
+        return render_template("forms/register-specialty.html")
