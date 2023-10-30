d3bce633b2b15c63d0326eb292af920367f5fdf7
IgorStrauss <engenheirostrauss2@gmail.com>
2023-10-30 11:50:58 -0300

    Formulario para cadastro de funcionarios ok

--- control_clinic/views.py
+++ control_clinic/views.py
@@ -5,3 +5,7 @@ def init_app(app):
     @app.route("/")
     def index():
         return render_template("base.html")
+
+    @app.route("/cadastro/funcionario", endpoint="register_employee")
+    def register_employee():
+        return render_template("forms/register-employee.html")
