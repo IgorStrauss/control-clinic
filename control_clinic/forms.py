47998e62a49e0d4e7744391f72ecbfe6ca393ee5
IgorStrauss <engenheirostrauss2@gmail.com>
2023-11-06 13:33:10 -0300

    Renderizando pagina listando 1 funcionario e pagina update funcionario

--- control_clinic/forms.py
+++ control_clinic/forms.py
@@ -43,6 +43,34 @@ class EmployeeForm(FlaskForm):
     )
 
 
+class EmployeeUpdateForm(FlaskForm):
+    firstname = StringField(
+        "Primer nombre",
+        validators=[
+            Length(min=3, max=45, message="Mínimo 3 caractere, máximo 45 caracteres"),
+        ],
+    )
+    lastname = StringField(
+        "Apellido",
+        validators=[
+            Length(min=3, max=45, message="Mínimo 3 caractere, máximo 45 caracteres"),
+        ],
+    )
+    email = StringField(
+        "E-mail",
+        validators=[
+            Email("Endereço de e-mail inválido."),
+        ],
+    )
+    phone = StringField(
+        "Teléfono móvil",
+        validators=[
+            Length(min=10, max=15,
+                   message="Mínimo 10 caracteres, máximo 15 caracteres"),
+        ],
+    )
+
+
 class SpecialtyForm(FlaskForm):
     name = StringField(
         "Especialidad",
--- control_clinic/views.py
+++ control_clinic/views.py
@@ -2,7 +2,8 @@ from flask import flash, redirect, render_template, request, url_for
 from sqlalchemy import desc
 from werkzeug.security import generate_password_hash
 
-from .forms import DoctorForm, EmployeeForm, PatientForm, SpecialtyForm
+from .forms import (DoctorForm, EmployeeForm, EmployeeUpdateForm, PatientForm,
+                    SpecialtyForm)
 from .models import (Doctor, Doctor_phone, Doctor_specialty, Employee_phone,
                      Employees, Patient, Patient_phone, db)
 
@@ -60,6 +61,47 @@ def init_app(app):
     def list_employees():
         employees = Employees.query.all()
         return render_template("employees/list_employees.html", employees=employees)
+
+    @app.route("/listar/funcionario/<int:id>", endpoint="list_employee")
+    def list_employee(id):
+        employee = Employees.query.get(id)
+        return render_template("employees/list_employee.html", employee=employee)
+
+    @app.route("/atualizar/funcionario/<int:id>", methods=["GET", "POST"], endpoint="update_employee")
+    def update_employee(id):
+        form = EmployeeUpdateForm()
+        employee = Employees.query.get(id)
+
+        # Carregue os dados do telefone associado ao funcionário
+        employee_phone = employee.phone
+
+        if form.validate_on_submit():
+            if form.firstname.data:
+                employee.firstname = form.firstname.data.upper()
+            if form.lastname.data:
+                employee.lastname = form.lastname.data.upper()
+            if form.email.data:
+                employee.email = form.email.data
+
+            # Atualize o número de telefone se um novo número for fornecido
+            if form.phone.data:
+                employee_phone.phone = form.phone.data
+
+            db.session.commit()  # Salva as atualizações no banco de dados
+
+            flash("Dados do funcionário atualizados com sucesso", "success")
+            return redirect(url_for("list_employee", id=id))
+
+        # Preenche os campos do formulário com os dados atuais do funcionário
+        form.firstname.data = employee.firstname
+        form.lastname.data = employee.lastname
+        form.email.data = employee.email
+
+        # Preenche o campo de telefone com o número de telefone atual
+        if employee_phone:
+            form.phone.data = employee_phone.phone
+
+        return render_template("employees/update_employees.html", form=form, employee=employee)
 
     @app.route("/cadastro/medico", methods=["GET", "POST"], endpoint="register_doctor")
     def register_doctor():
