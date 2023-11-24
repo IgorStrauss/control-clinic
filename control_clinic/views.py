0482b330aa9ef311123fac04dc3c17b962de65e4
IgorStrauss <engenheirostrauss2@gmail.com>
2023-11-24 10:43:59 -0300

    Pagicanao ok conforme descricao, com maximo de 5 itens por pagina

--- control_clinic/views.py
+++ control_clinic/views.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, url_for
+from flask import redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from control_clinic.models.medical_records_model import (ClinicCare,
@@ -12,7 +12,14 @@ def init_app(app):
         if login_required and current_user.is_anonymous:
             return redirect(url_for("login"))
 
-        clinic_care = ClinicCare.query.all()
+        """Paginacao da tabela ClinicCare"""
+        page = request.args.get("page", 1, type=int)
+        per_page = 5
+
+        clinic_care = ClinicCare.query.paginate(
+            page=page, per_page=per_page, error_out=False
+        )
+        # clinic_care = ClinicCare.query.all()
         patients = Patient.query.all()
         medical_records = MedicalRecords.query.all()
         return render_template("dash/dash_initial_page.html",
