0cf012dd41f3b78bdd50b06ea7c5b9e00e2b62a6
IgorStrauss <engenheirostrauss2@gmail.com>
2023-11-29 14:07:30 -0300

    Corrigindo secret_key na inicializacao do gunicorn junto ao csrf token

--- control_clinic/gunicorn_config.py
+++ control_clinic/gunicorn_config.py
@@ -1,3 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 workers = 4
 bind = "0.0.0.0:8000"
 threads = 2
+
+# Obtém a SECRET_KEY do ambiente
+secret_key = os.environ.get("SECRET_KEY")
+
+
+# Configuração específica do Flask-WTF
+raw_env = ["SECRET_KEY=" + os.getenv("SECRET_KEY")]
